524a92193675dc20e286f3e6469d7da65d033242
Januson J <68687958+Janus-chan@users.noreply.github.com>
2023-06-23 00:41:15 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-
 const {ethers,run,network} = require("hardhat")
 
 async function main(){
@@ -13,15 +6,6 @@ async function main(){
   const Manufacturer = await ManufacturerFactory.deploy()
   await Manufacturer.deployed()
   console.log(`Deployed contract to : ${Manufacturer.address}`);
-// console.log(network.config);
-
-// if(network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY){
-//   console.log("waiting for block ");
-//   await simpleStorage.deployTransaction.wait(6);
-//   await verify(simpleStorage.address,[])
-  
-// }
-
 
 
 }
